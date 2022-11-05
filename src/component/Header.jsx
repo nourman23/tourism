1171035ff7f008f81f3b0a5b517e81e3ca7fab62
nourman23 <nourmanzawahreh@gmail.com>
2022-11-05 19:26:22 +0200

    not found page

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -26,28 +26,3 @@ function Header (){
 }
   
 export default Header;
-// import { Outlet, NavLink } from "react-router-dom";
-  
-// const Layout = () => {
-//     return (
-//       <>
-//         <nav className="navbar navbar-expand-lg navbar-light bg-dark text-light">
-//         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-//             <li className="nav-item mx-3 p-3 ">
-//                 <NavLink   to="/">Home</NavLink>
-//             </li>
-//             <li className="nav-item mx-3 p-3">
-//                 <NavLink  to="/about">About Us</NavLink>
-//             </li>
-//             <li className="nav-item mx-3 p-3">
-//                 <NavLink  to="/contact">Contact Us</NavLink>
-//             </li>
-//     </ul>
-//         </nav>
-  
-//         <Outlet />
-//       </>
-//     )
-//   };
-  
-//   export default Layout;
---
+++ src/component/NoPage.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+  
+function NoPage (){
+    return ( 
+    <>
+     <h3>Page Is Not Found</h3>
+        </>
+    );
+    
+    
+}
+  
+export default NoPage;
