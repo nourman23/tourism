d3fb4ab14672c0e8612532d3aa71e46fdfed0045
nourman23 <nourmanzawahreh@gmail.com>
2022-11-14 17:46:56 +0300

    context task

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
-import './App.css';
-import Header from './component/Header';
-import NoPage from './component/NoPage';
-import Home from './component/Home';
-import TourDetails from './component/TourDetails';
-import Footer from './component/Footer';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-  Routes,
-  Route,
-} from 'react-router-dom';
+import React, { useContext } from "react";
+import "./App.css";
+import Header from "./component/Header";
+import NoPage from "./component/NoPage";
+import Home from "./component/Home";
+import TourDetails from "./component/TourDetails";
+import Footer from "./component/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route } from "react-router-dom";
+// import { AppContext } from "./context";
+import CitiesProvider from "./context";
 
 function App() {
   return (
     <div className="App">
-  <Header />
-  <Routes>
-
-
-<Route exact path='/' element={< Home />}></Route>
-<Route exact path='/Tour-details/:id' element={< TourDetails />}></Route>
-
-
-<Route path="*" element={<NoPage />} />
-
-</Routes>
-  <Footer />
-
+      <Header />
+      <CitiesProvider>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route
+            exact
+            path="/Tour-details/:id"
+            element={<TourDetails />}
+          ></Route>
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </CitiesProvider>
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
+
--- src/component/TourDetails.jsx
+++ src/component/TourDetails.jsx
@@ -1,32 +1,37 @@
 
-import React from 'react';
-import { useLocation } from "react-router-dom"
+import React ,{useContext} from 'react';
+import { AppContext } from '../context';
+
+import {useParams } from "react-router-dom"
 
   
 function TourDetails (){
-    const location = useLocation();
-    const data = location.state.data;
-    console.log(location.state, " useLocation Hook");
+    const {id} =useParams();
+	const { cities } = useContext(AppContext);
 
+    const city =cities.find(city => {
+        return city.id == id;
+    }) ;
+    console.log(city);
     return  (
          <>
 
          
-         <div className="container-fluid my-5  w-75">
-                <div className="row align-items-center">
+         <div className="container-fluid my-5  w-75"> 
+               <div className="row align-items-center">
                     <div className="col-md-6">
                         <h1 className="display-4 ">
-                        {data.name}
+                        {city.name}
                         </h1>
                         <div style={{ width:"600px" }}>
                             <p className="lead m-3" >
-                        {data.info}
+                        {city.info}
                         </p></div>
                         </div>
                         <div className="col-md-6 ">
                         <h1 className="display-4 " >
                             <div style={{ width:"600px" , height:"400px" }} className="m-5">
-                            <img src={data.image} alt="" width="100%" height="100%" /></div>
+                            <img src={city.image} alt="" width="100%" height="100%" /></div>
                         </h1>
                         </div>
                       
